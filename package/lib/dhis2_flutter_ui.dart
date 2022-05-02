d8296baad0a4860e4a5fc25bc19d742d3da8b173
Joseph Chingalo <profschingalo@gmail.com>
2022-05-02 17:50:27 +0300

    fix: 🐛 fix pas static analysis issue on score

--- package/lib/dhis2_flutter_ui.dart
+++ package/lib/dhis2_flutter_ui.dart
@@ -1,7 +1,36 @@
 library dhis2_flutter_ui;
 
-export './src/main_directive.dart' show 
-Application,Dhis2Option,InputFieldOption,InputField,DataElement
-,Dhis2EventDataValue,Dhis2Event,MetadataSyncItem,OrganisationUnit,ProgramIndicator,ProgramRuleAction,ProgramRuleVariable,ProgramRule,SystemInfo,
-CircularProcessLoader,MaterialCard,LineSeparator,
-SearchInput,BooleanInputFieldContainer,RadioInputFieldContainer,CheckBoxInputField,CoordinateInputFieldContainer,DateInputFieldContainer,InputClearIcon,NumericalInputFieldContainer,SelectInputField,TextInputFieldContainer,TrueOnlyInputFieldContainer,InputFieldContainer,PercentageInputFieldContainer,TimeInputFieldContainer,FormFieldInputIcon;
+export './src/main_directive.dart'
+    show
+        Application,
+        Dhis2Option,
+        InputFieldOption,
+        InputField,
+        DataElement,
+        Dhis2EventDataValue,
+        Dhis2Event,
+        MetadataSyncItem,
+        OrganisationUnit,
+        ProgramIndicator,
+        ProgramRuleAction,
+        ProgramRuleVariable,
+        ProgramRule,
+        SystemInfo,
+        CircularProcessLoader,
+        MaterialCard,
+        LineSeparator,
+        SearchInput,
+        BooleanInputFieldContainer,
+        RadioInputFieldContainer,
+        CheckBoxInputField,
+        CoordinateInputFieldContainer,
+        DateInputFieldContainer,
+        InputClearIcon,
+        NumericalInputFieldContainer,
+        SelectInputField,
+        TextInputFieldContainer,
+        TrueOnlyInputFieldContainer,
+        InputFieldContainer,
+        PercentageInputFieldContainer,
+        TimeInputFieldContainer,
+        FormFieldInputIcon;
