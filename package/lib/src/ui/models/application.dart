2cd67f42b99d5125d84c85e641eed5ef8be80f0b
megamind <echingalo@gmail.com>
2023-02-03 17:56:37 +0300

    docs(models): added documentations to the models

--- package/lib/src/ui/models/application.dart
+++ package/lib/src/ui/models/application.dart
@@ -1,12 +1,28 @@
+// Copyright (c) 2023, HISP Tanzania Developers.
+// All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
+///
+/// `Application` is a model class for the application
+///
 class Application {
+  /// This is the `String` representation of the application id.
   String id;
+
+  /// This is the `String` label for the application
   String label;
+
+  /// This is the `String` iconPath for the application
   String iconPath;
+
+  /// This is the `List` of user groups for the current logged in user
   List<String> userGroups;
+
+  /// This is a `Widget` page for the application
   Widget page;
 
+  /// This is the default constructor of the `Application` class
   Application(
       {required this.id,
       required this.label,
@@ -14,6 +30,7 @@ class Application {
       required this.page,
       this.userGroups = const []});
 
+  /// `Application.toString()` is the function to return the `String` representation of `Application`
   @override
   String toString() => label;
 }
--- package/lib/src/ui/models/input_field.dart
+++ package/lib/src/ui/models/input_field.dart
@@ -1,93 +1,132 @@
-import 'package:dhis2_flutter_ui/src/ui/models/input_field_option.dart';
-import 'package:flutter/material.dart';
+// Copyright (c) 2023, HISP Tanzania Developers.
+// All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:flutter/material.dart';
+import 'package:dhis2_flutter_ui/src/ui/models/input_field_option.dart';
+
+///
+/// `InputField` is the class for the input fields
+///
 class InputField {
+  /// this is the `String` representation of the id for the field's input
   String id;
+
+  /// this is the `String` representation of the display name for the field data input
   String name;
+
+  /// this is optional `String` translated display name for the input field
   String? translatedName;
+
+  /// this is a `String` description for the input field
   String? description;
+
+  /// this is a `String` translated description for the input field
   String? translatedDescription;
+
+  /// this is a `String` value type as per DHIS2 standards
   String valueType;
+
+  /// this is a `Color` for the input labels
   Color? labelColor;
+
+  /// this is a `Color` for the input borders
   Color? inputColor;
+
+  /// this is a background `Color` for the input field
   Color? background;
+
+  /// this is a `bool` that specifies wether `InputField` should render its option as radio or not
   bool? renderAsRadio;
+
+  /// this is a `bool` that specifies wether `InputField` is read only or not
   bool? isReadOnly;
+
+  /// this is a `bool` that specifies wether `InputField` should be rendered as password input
   bool? isPasswordField;
+
+  /// this is a `bool` that specifies wether `InputField` value should be capitalized or not
   bool? shouldCapitalize;
+
+  /// this is a `bool` that specifies wether `InputField` should allow future dates if its of type `DATE`
   bool? allowFuturePeriod;
-  bool? showCountryLevelTree;
+
+  /// this is a `bool` that specifies wether `InputField` should allow past dates if its of type `DATE`
   bool? disablePastPeriod;
+
+  /// this is a `bool` that specifies wether `InputField` should show an error
   bool? hasError;
+
+  /// this is an `int` that specifies years that is minimum years that can be captured by the `InputField`
   int? minYear;
+
+  /// this is an `int` that specifies years that is maximum years that can be captured by the `InputField`
   int? maxYear;
+
+  /// this is an `int` that specifies the minimum number of months that can be captured by the `InputField`
   int? numberOfMonth;
-  bool? shouldUserCustomAgeLimit;
+
+  /// this is a `String` label that is displayed as a suffix to the `InputField`
   String? suffixLabel;
+
+  /// this is a `String` label that is displayed as a prefix to the `InputField`
   String? prefixLabel;
+
+  /// this is a `String` representation of a hint to the `InputField`
   String? hint;
+
+  /// this is a `String` representation of a translated hint to the `InputField`
   String? translatedHint;
+
+  /// this is a `List` of `InputFieldOption` for the `InputField`
   List<InputFieldOption>? options;
+
+  /// this is a `bool` that indicates wether an input has a sub-input field
   bool? hasSubInputField;
+
+  /// this is an `InputField` that represents a sub-input field
   InputField? subInputField;
-  List<int>? allowedSelectedLevels;
-  List<String>? filteredPrograms;
+
+  /// this is a `DateTime` that represents a minimum date allowed to be captured by a `DATE` field
   DateTime? minDate;
+
+  /// this is a `DateTime` that represents a maximum date allowed to be captured by a `DATE` field
   DateTime? maxDate;
 
+  ///
+  /// This is the default constructor to the `InputField`
+  ///
   InputField({
     required this.id,
     required this.name,
     required this.valueType,
-    this.hasSubInputField,
-    this.description,
-    this.inputColor,
-    this.labelColor,
-    this.background,
-    this.renderAsRadio,
-    this.isReadOnly,
-    this.isPasswordField,
-    this.shouldCapitalize,
-    this.options,
+    this.translatedName = '',
+    this.description = '',
+    this.hint = '',
+    this.translatedHint = '',
+    this.translatedDescription = '',
+    this.prefixLabel = '',
+    this.suffixLabel = '',
     this.subInputField,
-    this.allowedSelectedLevels,
-    this.allowFuturePeriod,
-    this.disablePastPeriod,
-    this.translatedName,
-    this.translatedDescription,
-    this.hint,
-    this.translatedHint,
     this.maxYear,
     this.minYear,
     this.numberOfMonth,
-    this.prefixLabel,
-    this.suffixLabel,
-    this.filteredPrograms,
-    this.hasError,
-    this.shouldUserCustomAgeLimit,
-    this.showCountryLevelTree,
     this.minDate,
-    this.maxDate
-  }) {
-    isPasswordField = isPasswordField ?? false;
-    shouldCapitalize = shouldCapitalize ?? false;
-    showCountryLevelTree = showCountryLevelTree ?? false;
-    allowedSelectedLevels = allowedSelectedLevels ?? [];
-    allowFuturePeriod = allowFuturePeriod ?? false;
-    disablePastPeriod = disablePastPeriod ?? false;
-    isReadOnly = isReadOnly ?? false;
-    description = description ?? '';
-    hint = hint ?? '';
-    hasSubInputField = hasSubInputField ?? false;
-    renderAsRadio = renderAsRadio ?? false;
-    options = options ?? [];
-    filteredPrograms = filteredPrograms ?? [];
-    inputColor = inputColor ?? Colors.black;
-    labelColor = labelColor ?? const Color(0xFF1A3518);
-    background = background ?? Colors.transparent;
-    suffixLabel = suffixLabel ?? '';
-  }
+    this.maxDate,
+    this.inputColor = Colors.black,
+    this.labelColor = const Color(0xFF1A3518),
+    this.background = Colors.transparent,
+    this.options = const [],
+    this.renderAsRadio = false,
+    this.isReadOnly = false,
+    this.hasSubInputField = false,
+    this.isPasswordField = false,
+    this.shouldCapitalize = false,
+    this.hasError = false,
+    this.allowFuturePeriod = false,
+    this.disablePastPeriod = false,
+  });
 
+  /// `InputField.toString()` is the method to return the `String` representation of the `InputField`
   @override
   String toString() {
     return '$id - $name - $isReadOnly';
