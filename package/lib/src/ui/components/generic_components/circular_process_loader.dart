d820ec9adcd8b2fa9d5726207d96148c4bb35b55
megamind <echingalo@gmail.com>
2023-02-04 13:42:10 +0300

    docs(generic_components): added documentation on the generic components

--- package/lib/src/ui/components/generic_components/circular_process_loader.dart
+++ package/lib/src/ui/components/generic_components/circular_process_loader.dart
@@ -1,9 +1,26 @@
+// Copyright (c) 2023, HISP Tanzania Developers.
+// All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
+///
+/// `CircularProcessLoader` is a widget for loading indications
+///
 class CircularProcessLoader extends StatelessWidget {
-  const CircularProcessLoader({Key? key, this.color, this.size})
-      : super(key: key);
+  ///
+  /// This is a default constructor for the `CircularProcessLoader`
+  /// It accepts the loader color and size
+  ///
+  const CircularProcessLoader({
+    Key? key,
+    this.color,
+    this.size,
+  }) : super(key: key);
+
+  /// this is a `Color` for the loading indicator icon
   final Color? color;
+
+  /// this is a `double` size for the loading indicator
   final double? size;
 
   @override
@@ -18,11 +35,13 @@ class CircularProcessLoader extends StatelessWidget {
             children: [
               CircularProgressIndicator(
                 strokeWidth: size ?? 4,
-                valueColor: AlwaysStoppedAnimation(color ?? Colors.white),
-              )
+                valueColor: AlwaysStoppedAnimation(
+                  color ?? Colors.white,
+                ),
+              ),
             ],
           ),
-        )
+        ),
       ],
     );
   }
--- package/lib/src/ui/components/generic_components/line_separator.dart
+++ package/lib/src/ui/components/generic_components/line_separator.dart
@@ -1,12 +1,26 @@
+// Copyright (c) 2023, HISP Tanzania Developers.
+// All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
+///
+/// `LineSeparator` is the widget for the a line
+///
 class LineSeparator extends StatelessWidget {
+  ///
+  /// this is the default constructor for the `LineSeparator`
+  /// it accepts the color and height for the line
+  ///
   const LineSeparator({
     Key? key,
     required this.color,
     this.height = 2.0,
   }) : super(key: key);
+
+  /// this is a `Color` for the line
   final Color color;
+
+  /// this is a `double` for the height of the line, it can be considered as a thickness of line
   final double height;
 
   @override
@@ -14,12 +28,13 @@ class LineSeparator extends StatelessWidget {
     return Container(
       height: height,
       decoration: BoxDecoration(
-          border: Border(
-        bottom: BorderSide(
-          width: height,
-          color: color,
+        border: Border(
+          bottom: BorderSide(
+            width: height,
+            color: color,
+          ),
         ),
-      )),
+      ),
     );
   }
 }
